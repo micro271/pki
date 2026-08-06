e7f3d80a4d905d9b9b0251d29c204783760a926d
Matias Zelaya <128877338+micro271@users.noreply.github.com>
2026-08-06 10:26:40 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
+use crate::repository::Repository;
+use tokio::net::{UnixListener, UnixStream};
+
 pub mod models;
 pub mod repository;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    dotenv::dotenv().ok();
+
+    let db = std::env::var("DATABASE").unwrap();
+    let userdb = std::env::var("USER_DB").unwrap();
+    let passwddb = std::env::var("PASS_DB").unwrap();
+    let host = std::env::var("HOST_DB").unwrap();
+    let port = std::env::var("PORT_DB").unwrap();
+    let socket_path = std::env::var("FILE_SOCKET").unwrap_or("/tmp/rust-app.socket".to_string());
+
+    if let Err(er) = std::fs::remove_file(&socket_path) {
+        tracing::error!("Delete file ({socket_path}) failed: {er}");
+    }
+
+    let (stm, socket) = UnixListener::bind(socket_path)?.accept().await?;
+
+    tracing::info!("Listening {socket:?}");
+
+    tokio::spawn(socket_handler(stm));
+
+    let url = format!("postgres://{userdb}:{passwddb}@{host}:{port}/{db}");
+
+    /* postgres:///mydb?host=/var/run/postgresql */
 
     Ok(())
+}
+
+async fn socket_handler(stm: UnixStream) {
+    todo!()
 }
